3af61cde4fce8eb9cd7bf95d40dff83f4eef86d5
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-10-20 22:22:30 -0300

    Create todo.model.ts

---
+++ src/models/todo.model.ts
@@ -0,0 +1,11 @@
+export class Todo{
+    // public todo: String = "";
+    // public done: boolean = false;
+    // public id: number = 0;
+    
+    constructor(
+        public id: Number,
+        public title: String,
+        public done: Boolean
+    ) { }
+}
